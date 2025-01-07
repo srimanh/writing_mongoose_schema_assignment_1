c4cf0a4d49c1a662a2c55d2576f62e74a2d8749e
unknown <srimandgl2004@gmail.com>
2025-01-07 18:35:39 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { resolve } = require('path');
-
+const mongoose = require('mongoose');
+const User = require('./schema');
 const app = express();
 const port = 3010;
 
@@ -9,6 +10,31 @@ app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+mongoose.connect('mongodb://localhost:27017/user_management', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error('MongoDB Connection Failed:', err));
+
+
+const newUser = new User({
+  username: 'JohnDoe',
+  email: 'John@example.com',
+  password: 'HashedPasswordHere',
+  roles: ['admin'],
+  profile: {
+    firstName: 'JJohn',
+    lastName: 'DDoe',
+    age: 30
+  }
+});
+
+newUser.save()
+  .then(user => console.log('User created:', user))
+  .catch(err => console.error('Error creating user:', err));
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
